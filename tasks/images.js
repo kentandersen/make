e0eccf64f4a680e108949208f29be1a9328867e7
Kent Andersen <kent.andersen2@sparebank1.no>
2014-04-03 11:09:30 +0200

    added faile on error config image task

--- tasks/images.js
+++ tasks/images.js
@@ -6,52 +6,60 @@ var npmBin = require("npm-bin");
 var ProgressBar = require('progress');
 var utils = require("../lib/utils");
 
+var defaultOptions = {
+    failOnError: true
+};
+
 /**
  * options
  *   inputDir:      [string] directory with files to be optimized
  *   outputDir:     [string] directory to copy optimized image files
  */
-var done = function (isSuccess) {
-    if(isSuccess) {
-        utils.success();
-    } else {
-        utils.fail();
-    }
-}
-
-
-var optimizePngImages = function(pngs) {
-    if(pngs.length > 0) {
-        console.log("Handeling pngs");
-        var bar = new ProgressBar(':bar', { total: pngs.length });
-
-        var success = _.every(pngs, function(png) {
-            bar.tick();
-            return npmBin('optipng', ['-strip all', '-o7', '-zm1-9', '-clobber', png], {silent: true}).code === 0;
-        });
-
-        done(success);
-    }
-};
-
-var optimizeSvgImages = function(svgs) {
-    if(svgs.length > 0) {
-        console.log("Handeling svgs");
-        var bar = new ProgressBar(':bar', { total: svgs.length });
-
-        var success = _.every(svgs, function(svg) {
-            bar.tick();
-            return npmBin('svgo', svg, {silent: true}).code === 0;
-        });
-
-        done(success);
-    }
-};
-
 var buildimages = function(options) {
-
+    _.defaults(options, defaultOptions);
     var inputDir = options.inputDir;
     var outputDir = options.outputDir;
+
+    var done = function (isSuccess) {
+        if(isSuccess) {
+            utils.success();
+
+        } else if(!options.failOnError) {
+            utils.skipped();
+
+        } else {
+            utils.fail();
+        }
+    }
+
+
+    var optimizePngImages = function(pngs) {
+        if(pngs.length > 0) {
+            console.log("Handeling pngs");
+            var bar = new ProgressBar(':bar', { total: pngs.length });
+
+            var success = _.every(pngs, function(png) {
+                bar.tick();
+                return npmBin('optipng', ['-strip all', '-o7', '-zm1-9', '-clobber', png], {silent: true}).code !== 0;
+            });
+
+            done(success);
+        }
+    };
+
+    var optimizeSvgImages = function(svgs) {
+        if(svgs.length > 0) {
+            console.log("Handeling svgs");
+            var bar = new ProgressBar(':bar', { total: svgs.length });
+
+            var success = _.every(svgs, function(svg) {
+                bar.tick();
+                return npmBin('svgo', svg, {silent: true}).code === 0;
+            });
+
+            done(success);
+        }
+    };
 
     utils.section('Copying resources ' + inputDir + ' → ' + outputDir);
 
