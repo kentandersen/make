4981a391111782e525a02cea40d3fdf630c1bef3
Kent Andersen <kent.andersen2@sparebank1.no>
2014-04-08 15:38:09 +0200

    added support for configObject for custom command line arguments

--- tasks/require.js
+++ tasks/require.js
@@ -1,17 +1,32 @@
+var _ = require("underscore");
 var utils = require("../lib/utils");
 
 /**
  * options
  *   outputFile:    [array] file to save to
- *   config:        [string] path to rjs config file
+ *   configFile:    [string] path to rjs config file
+ *   config:        [object] extra command line options
  */
+
+
+var generateConfig = function(config) {
+    if(!config) {
+        return "";
+    }
+
+    var args = _.map(config, function(value, key) {
+        return key+"="+value;
+    });
+    return args.join(" ");
+};
 
 var requirejsTask = function(options) {
     var outputFile = options.outputFile;
-    var rjsConfig = options.config;
+    var rjsConfig = options.configFile;
+    var args = generateConfig(options.config);
 
     utils.section('Building JavaScript → ' + outputFile);
-    utils.bin('r.js', ['-o ' + rjsConfig, 'out=' + outputFile]);
+    utils.bin('r.js', ['-o ' + rjsConfig, 'out=' + outputFile, args]);
 };
 
 requirejsTask.description = "Optimizes all usage of bear";
