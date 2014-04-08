af1832672a94887f9bfca64e9873776343c325b7
Kent Andersen <kent.andersen2@sparebank1.no>
2014-04-08 15:36:58 +0200

    renamed from config to configFile

--- tasks/jshint.js
+++ tasks/jshint.js
@@ -8,20 +8,20 @@ var defaultOptions = {
 
 /**
  * options
- *   files:     [array] list of files to check
- *   config:    [string] path to json config file
- *   exclude:   [string] part of file path to be excluded
+ *   files:         [array] list of files to check
+ *   configFile:    [string] path to json config file
+ *   exclude:       [string] part of file path to be excluded
  */
+
 
 var jsHintTask = function(options) {
     _.defaults(options, defaultOptions);
 
-
+    var configFile = options.configFile;
     var files = options.files;
     var excludes = options.exclude;
     excludes = _.isArray(excludes) ? excludes : [excludes];
 
-    var config = options.config;
 
     files = _.filter(files, function(file) {
         return _.every(excludes, function(exclude) {
@@ -30,7 +30,7 @@ var jsHintTask = function(options) {
     });
 
     utils.section('Running JSHint ' + files.length + ' files');
-    utils.bin('jshint', ['--config ' + config, files.join(' ')]);
+    utils.bin('jshint', ['--config ' + configFile, files.join(' ')]);
 };
 
 jsHintTask.description = "Jshint verification for all *.js files"
